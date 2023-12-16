48609ffe80e3a6ae658f584b38a70d0ac6e074da
Thomas Zub <thomas.zub@outlook.de>
2023-12-16 09:10:54 +0100

    Invert conditions and remove else branches

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,25 +3,20 @@ package games;
 public class FizzBuzz {
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        if (input <= 0 || input > 100) {
             throw new OutOfRangeException();
         }
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+        if (input % 5 == 0) {
+            return "Buzz";
+        }
+        return input.toString();
     }
 }
